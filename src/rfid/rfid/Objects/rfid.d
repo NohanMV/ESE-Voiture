74e9a5a501dcc142176cc926eaa95a8784d54154
NohanMV <nohanmv@gmail.com>
2024-02-08 18:41:58 +0100

    Ignorer les fichiers mentionnés dans le .gitignore

--- src/rfid/rfid/Objects/rfid.d
+++
@@ -1,6 +0,0 @@
-.\objects\rfid.o: RFID.c
-.\objects\rfid.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_USART.h
-.\objects\rfid.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_Common.h
-.\objects\rfid.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\rfid.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\rfid.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
