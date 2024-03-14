eb2b311e04c2b5e48e6a474669a693751db403a2
loic-vivies <159029310+loic-vivies@users.noreply.github.com>
2024-03-14 17:51:04 +0100

    Add files via upload

---
+++ src/Supervision/Objects/gpio.d
@@ -0,0 +1,7 @@
+.\objects\gpio.o: GPIO.c
+.\objects\gpio.o: LPC17xx.h
+.\objects\gpio.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm3.h
+.\objects\gpio.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\gpio.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\objects\gpio.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\objects\gpio.o: C:\Keil_v5\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
---
+++ src/Supervision/Objects/it2r2.d
@@ -0,0 +1,9 @@
+.\objects\it2r2.o: IT2R2.c
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\PACK\Keil\MDK-Middleware\7.0.0\emWin\Include\GUI.h
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\PACK\Keil\MDK-Middleware\7.0.0\emWin\Include\GUI_ConfDefaults.h
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\PACK\Keil\MDK-Middleware\7.0.0\emWin\Include\GUIConf.h
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\PACK\Keil\MDK-Middleware\7.0.0\emWin\Include\GUI_Type.h
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\PACK\Keil\MDK-Middleware\7.0.0\emWin\Include\LCD.h
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\PACK\Keil\MDK-Middleware\7.0.0\emWin\Include\Global.h
+.\objects\it2r2.o: D:\Programmes\Keil_uV5\ARM\PACK\Keil\MDK-Middleware\7.0.0\emWin\Include\GUI_Version.h
