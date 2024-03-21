0916a1eed98ad8c0891868252409282f12040d68
LeoVal2 <158590042+LeoVal2@users.noreply.github.com>
2024-03-21 23:37:43 +0100

    amélioration du programme de vérification liaison esp32 K210

--- src/MAIXDUINO/IA/programe_test_bluetooth/connexion_esp32_K210.py
+++ src/MAIXDUINO/IA/programe_test_bluetooth/connexion_esp32_K210.py
@@ -13,4 +13,5 @@ class wifi():
     fm.register(27,fm.fpioa.SPI1_SCLK, force=True)#sclk
     fm.register(25,fm.fpioa.GPIOHS10)#cs
     nic = network.ESP32_SPI(cs=fm.fpioa.GPIOHS10, rst=fm.fpioa.GPIOHS11, rdy=fm.fpioa.GPIOHS12, spi=1)
-
+    
+print("ESP32_SPI firmware version:", wifi.nic.version())
