a3ec4d6f6d0deadf40eb788e68d0dbd2b8fa77fa
nsyak <145785632+nsyak@users.noreply.github.com>
2024-03-22 12:53:28 +0100

    Programme CAN transmission/réception affichage LCD

---
+++ src/MCB1700/BusCAN/RTE/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'CAN_IT2R' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_DEVICE_STARTUP_LPC17XX      /* Device Startup for NXP17XX */
+#define RTE_Drivers_CAN1                /* Driver CAN1 */
+        #define RTE_Drivers_CAN2                /* Driver CAN2 */
+#define RTE_Drivers_SPI0                /* Driver SPI0 */
+        #define RTE_Drivers_SPI1                /* Driver SPI1 */
+
+#endif /* RTE_COMPONENTS_H */
