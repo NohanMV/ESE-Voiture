e297cbeb0bcc32d3d89eaaf9525397f28a85e546
loic-vivies <159029310+loic-vivies@users.noreply.github.com>
2024-03-26 16:26:44 +0100

    Delete src/MCB1700 directory

--- src/MCB1700/pilotage/TIMER.h
+++
@@ -1,7 +0,0 @@
-void initTimer0(int prescaler, int N);
-	
-void signalCarreTimer0 (int prescaler, int N);
-
-void CompteurEvenement(void);
-
-void commande3CouleursBandeLEDS(int DCrouge,int DCvert, int DCbleu);
