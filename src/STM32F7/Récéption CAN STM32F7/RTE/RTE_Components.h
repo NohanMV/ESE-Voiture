9ac27a15c1beb1178038bd1d004e03d6ed4dd94b
nsyak <145785632+nsyak@users.noreply.github.com>
2024-04-02 18:02:51 +0200

    CAN Réception STM32F7 suites à envoi MCB1700

---
+++ src/STM32F7/Récéption CAN STM32F7/RTE/RTE_Components.h
@@ -0,0 +1,42 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Example' 
+ * Target:  'STM32F746 Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_DEVICE_FRAMEWORK_CLASSIC
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_DMA2D
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_I2C
+#define RTE_DEVICE_HAL_LTDC
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_SDRAM
+#define RTE_DEVICE_HAL_UART
+#define RTE_DEVICE_STARTUP_STM32F7XX    /* Device Startup for STM32F7 */
+#define RTE_Drivers_CAN1                /* Driver CAN1 */
+        #define RTE_Drivers_CAN2                /* Driver CAN2 */
+        #define RTE_Drivers_CAN3                /* Driver CAN3 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+        #define RTE_Drivers_USART5              /* Driver USART5 */
+        #define RTE_Drivers_USART6              /* Driver USART6 */
+        #define RTE_Drivers_USART7              /* Driver USART7 */
+        #define RTE_Drivers_USART8              /* Driver USART8 */
+#define RTE_Graphics_Core               /* Graphics Core */
+#define RTE_Graphics_Touchscreen        /* Graphics Input Device Touchscreen */
+
+#endif /* RTE_COMPONENTS_H */
