e3d9578e389b2e9a5d7a74ca43c955def08441f4
nsyak <145785632+nsyak@users.noreply.github.com>
2024-04-05 07:25:46 +0200

    Récéption STM32F4 après envoi de la MCB1700

---
+++ src/STM32F4-AVANT/STM32F4_CAN_reception/main - Copie.c
@@ -0,0 +1,325 @@
+/**
+  ******************************************************************************
+  * @file    Templates/Src/main.c 
+  * @author  MCD Application Team
+  * @brief   STM32F4xx HAL API Template project 
+  *
+  * @note    modified by ARM
+  *          The modifications allow to use this file as User Code Template
+  *          within the Device Family Pack.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h" 
+#include "osObjects.h"
+#include "Driver_CAN.h"
+#include "Board_LED.h"
+
+#ifdef _RTE_
+#include "RTE_Components.h"             // Component selection
+#endif
+#ifdef RTE_CMSIS_RTOS2                  // when RTE component CMSIS RTOS2 is used
+#include "cmsis_os2.h"                  // ::CMSIS:RTOS2
+#endif
+
+#ifdef RTE_CMSIS_RTOS2_RTX5
+/**
+  * Override default HAL_GetTick function
+  */
+uint32_t HAL_GetTick (void) {
+  static uint32_t ticks = 0U;
+         uint32_t i;
+
+  if (osKernelGetState () == osKernelRunning) {
+    return ((uint32_t)osKernelGetTickCount ());
+  }
+
+  /* If Kernel is not running wait approximately 1 ms then increment 
+     and return auxiliary tick counter value */
+  for (i = (SystemCoreClock >> 14U); i > 0U; i--) {
+    __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP();
+    __NOP(); __NOP(); __NOP(); __NOP(); __NOP(); __NOP();
+  }
+  return ++ticks;
+}
+
+#endif
+
+/** @addtogroup STM32F4xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup Templates
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+extern ARM_DRIVER_CAN Driver_CAN2;
+osThreadId ID_writeCAN;
+osThreadId ID_readCAN;
+
+void envoi_CAN(void const *arg);
+void lecture_CAN(void const *argument);
+
+void CAN_callback(uint32_t obj_idx, uint32_t event){
+    switch (event){
+			case ARM_CAN_EVENT_SEND_COMPLETE:
+				osSignalSet(ID_writeCAN, 0x01);
+				break;
+			//case ARM_CAN_EVENT_RECEIVE:
+				//osSignalSet(ID_readCAN, 0x01);
+				//break;
+    }
+}
+
+
+void lecture_CAN(void const *argument){
+	ARM_CAN_MSG_INFO   rx_msg_info;
+	uint8_t data_buf[1], retour;
+	while(1){
+		osSignalWait(0x01, osWaitForever);		// sommeil en attente réception
+		LED_On(1);
+		Driver_CAN2.MessageRead(0, &rx_msg_info, data_buf, 1);
+		retour = data_buf[0];
+		//osSignalSet(ID_writeCAN, 0x04);
+		}
+	}
+
+void envoi_CAN(void const *arg){
+	ARM_CAN_MSG_INFO trame_envoi;
+	uint8_t data[2];
+	trame_envoi.rtr = 0;
+	while(1){
+		osDelay(osWaitForever);
+		trame_envoi.id = ARM_CAN_STANDARD_ID(0x01);
+		//osSignalWait(0x04, osWaitForever);
+		data[0] = 0x19;
+		data[1] = 0xAA;
+		Driver_CAN2.MessageSend(2, &trame_envoi, data, 2);
+		osSignalWait(0x01, osWaitForever);
+		osDelay(osWaitForever);
+	}
+}
+
+osThreadDef(envoi_CAN, osPriorityNormal, 1, 0);
+osThreadDef(lecture_CAN, osPriorityNormal, 1, 0);
+
+void InitCAN(void){
+	Driver_CAN2.Initialize(NULL, CAN_callback);
+	Driver_CAN2.PowerControl(ARM_POWER_FULL);
+	
+	Driver_CAN2.SetMode(ARM_CAN_MODE_INITIALIZATION);
+	Driver_CAN2.SetBitrate( ARM_CAN_BITRATE_NOMINAL,
+													125000,
+													ARM_CAN_BIT_PROP_SEG(5U)   |         // Set propagation segment to 5 time quanta
+                          ARM_CAN_BIT_PHASE_SEG1(1U) |         // Set phase segment 1 to 1 time quantum (sample point at 87.5% of bit time)
+                          ARM_CAN_BIT_PHASE_SEG2(1U) |         // Set phase segment 2 to 1 time quantum (total bit is 8 time quanta long)
+                          ARM_CAN_BIT_SJW(1U));                // Resynchronization jump width is same as phase segment 2
+                          
+	Driver_CAN2.ObjectSetFilter(1, ARM_CAN_FILTER_ID_EXACT_ADD, ARM_CAN_STANDARD_ID(0x128), 0);
+	
+	Driver_CAN2.ObjectConfigure(2, ARM_CAN_OBJ_TX);				
+	Driver_CAN2.ObjectConfigure(1, ARM_CAN_OBJ_RX);
+	Driver_CAN2.SetMode(ARM_CAN_MODE_NORMAL);					// fin init
+}
+
+int main(void){
+
+  /* STM32F4xx HAL library initialization:
+       - Configure the Flash prefetch, Flash preread and Buffer caches
+       - Systick timer is configured by default as source of time base, but user 
+             can eventually implement his proper time base source (a general purpose 
+             timer for example or other time source), keeping in mind that Time base 
+             duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+             handled in milliseconds basis.
+       - Low Level Initialization
+     */
+  HAL_Init();
+
+  /* Configure the system clock to 168 MHz */
+  SystemClock_Config();
+  SystemCoreClockUpdate();
+
+  /* Add your application code here
+     */
+	
+
+#ifdef RTE_CMSIS_RTOS2	// A commenter si utilisation RTOS
+  /* Initialize CMSIS-RTOS2 */
+  osKernelInitialize ();
+
+  /* Create thread functions that start executing, 
+  Example: osThreadNew(app_main, NULL, NULL); */
+
+  /* Start thread execution */
+  osKernelStart();
+#endif
+	InitCAN();
+	LED_Initialize();
+  osKernelInitialize();
+	//LED_On(1);
+	ID_writeCAN = osThreadCreate(osThread(envoi_CAN), NULL);
+	//ID_readCAN = osThreadCreate(osThread(lecture_CAN), NULL);
+  osKernelStart();
+	osDelay(osWaitForever);
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 168000000
+  *            HCLK(Hz)                       = 168000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 336
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    /* Initialization Error */
+    Error_Handler();
+  }
+
+  /* STM32F405x/407x/415x/417x Revision Z devices: prefetch is supported */
+  if (HAL_GetREVID() == 0x1001)
+  {
+    /* Enable the Flash prefetch */
+    __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  /* User may add here some code to deal with this error */
+  while(1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
