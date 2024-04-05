1037e8bc6d88b435a74ff3b62abf5dfd49869152
Issadiallo223 <159419407+Issadiallo223@users.noreply.github.com>
2024-04-05 09:08:08 +0200

    Version fonctionnel GPS avec callback/RTOS
    intégration de la fonction callback non finit et début RTOS 
    On refait le commit de la version d'avant à cette date, car le commit de la veille n'a pas fonctionné.

--- src/GPS/Objects/uart_lpc17xx.d
+++ src/GPS/Objects/uart_lpc17xx.d
@@ -14,5 +14,5 @@
 .\objects\uart_lpc17xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
 .\objects\uart_lpc17xx.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.4.0\RTE_Driver\PIN_LPC17xx.h
 .\objects\uart_lpc17xx.o: C:\Keil_v5\ARM\PACK\Keil\LPC1700_DFP\2.4.0\RTE_Driver\GPDMA_LPC17xx.h
-.\objects\uart_lpc17xx.o: U:\ESE-VOITURE\ESE-Voiture-STM32F4-AVANT\ESE-Voiture-STM32F4-AVANT\src\GPS\RTE\Device\LPC1768\RTE_Device.h
-.\objects\uart_lpc17xx.o: U:\ESE-VOITURE\ESE-Voiture-STM32F4-AVANT\ESE-Voiture-STM32F4-AVANT\src\GPS\RTE\RTE_Components.h
+.\objects\uart_lpc17xx.o: U:\S4\GPSservice\GPS\RTE\Device\LPC1768\RTE_Device.h
+.\objects\uart_lpc17xx.o: U:\S4\GPSservice\GPS\RTE\RTE_Components.h
--- src/GPS/main.c
+++ src/GPS/main.c
@@ -1,24 +1,33 @@
-#include "LPC17xx.h"                    // Device header
 #include "Driver_USART.h"               // ::CMSIS Driver:USART
-#include "Board_GLCD.h"                 // ::Board Support:Graphic LCD
 #include "GLCD_Config.h"                // Keil.MCB1700::Board Support:Graphic LCD
-#include "RTE_Components.h"             // Component selection
-#include <stdio.h>      
+#include "Board_GLCD.h"                 // ::Board Support:Graphic LCD
+#include "stdio.h"
 #include <stdlib.h>
-#include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
-#include "fonctionGPS.h"
 #include <string.h>
 
-//TX P0.15
-//RX P0.16
-extern ARM_DRIVER_USART Driver_USART1;
-extern GLCD_FONT GLCD_Font_16x24 ; 
+#define GPS_DATA_SIZE 70
 
-void Init_UART(void)
-{
-	Driver_USART1.Initialize(NULL);
+void myUSART_callback(uint32_t event);
+
+extern ARM_DRIVER_USART Driver_USART1;
+extern GLCD_FONT GLCD_Font_16x24 ;
+extern GLCD_FONT GLCD_Font_6x8 ;
+
+char dataGPS[80]; // Tableau pour stocker la trame GPS
+volatile int i = 0;
+
+int fin_drapeau = 0 ;
+
+void traitement_GPS(const char *dataGPS[]){
+	
+	
+	
+}
+
+void Init_UART(void){
+	Driver_USART1.Initialize(myUSART_callback);
 	Driver_USART1.PowerControl(ARM_POWER_FULL);
-	Driver_USART1.Control(	ARM_USART_MODE_ASYNCHRONOUS |
+	Driver_USART1.Control(ARM_USART_MODE_ASYNCHRONOUS |
 							ARM_USART_DATA_BITS_8		|
 							ARM_USART_STOP_BITS_1		|
 							ARM_USART_PARITY_NONE		|
@@ -29,114 +38,72 @@ void Init_UART(void)
 }
 
 
-void traitementGPS(unsigned char *dataGPS){
-	
-    char trameID[4] ; // récupère l'identifiant de la trame GPS
-		char inutile[100]; // stocke les données de la trame inutile
-	  char coordonees[50]; // stocke les données de la trame utile
-	
-    double longitude,latitude; // récupere données GPS
-	  char Tab_longitude[20], Tab_latitude[20]; // affichage valeurs
-
-    char dirLong,dirLat; // récupère direction GPS
-		char Tab_dirLong[5], Tab_dirLat[5]; // affichage valeurs
-	
-    int i; 
-	
-		for(i=0;dataGPS[i]!=0;i++)// remplace les , par un espace dans la chaine
-		{
-				if (dataGPS[i]==',')
-						dataGPS[i]=' ';
-		}
-		
-    
-		// Utilisation de sscanf pour extraire les entiers du tableau de caractères
-		//             $GPGGA,123519,4807.038,N,01131.324,E,1,08,0.9,545.4,M,46.9,M, , *42
-		sscanf(dataGPS,"GP%s %s %lf %c %lf %c %s", trameID,inutile,&longitude,&dirLong,&latitude,&dirLat,inutile);
-	
-		sprintf(Tab_longitude,"%lf",longitude);
-		sprintf(Tab_latitude,"%lf",latitude);
-		sprintf(Tab_dirLong,"%c",dirLong);
-		sprintf(Tab_dirLat,"%c",dirLat);
-    
-		// méthode qui permet de vérifier si une trameID nous est utile
-		if(checkIDtrame(trameID) == 0) // si oui GGA
-			{
-			GLCD_DrawString(10, 50,"Longitude:");	
-			GLCD_DrawString(150, 50,Tab_longitude);
-			GLCD_DrawString(10, 90,"Latitude:");	
-			GLCD_DrawString(150, 90,Tab_latitude);
-			GLCD_DrawString(10, 130,"Direction:");	
-			GLCD_DrawString(180, 130,Tab_dirLong);	
-			GLCD_DrawString(200, 130,Tab_dirLat);
-		  GLCD_DrawString(10, 10,"Trame ID:");		
-			GLCD_DrawString(150, 10,trameID);
-			delay(2000);
-
-			}
-		else // si non
-			{
-			GLCD_DrawString(10, 44,trameID);
-		}
-}
-	
-
-int main(void)
+void myUSART_callback(uint32_t event)
 {
-	unsigned char dataGPS[100], test[1], test2[1];
-	int i;
+  if (event &  ARM_USART_EVENT_RECEIVE_COMPLETE) {
+	
+			if (dataGPS[i] == 0x0A) // LF
+				{
+					//GLCD_DrawString(0,3*24,"fin");
+					fin_drapeau = 1 ;
+				}
+				else 
+				{
+					
+					if (dataGPS[i] == ',') // LF
+					{
+						dataGPS[i] = ' ';
 
+					}
+					
+					if ( dataGPS[0] == '$' )
+					{
+						i++;
+					}
+					Driver_USART1.Receive(&dataGPS[i],1); 
+				}
+			}
+					
+//				if ( gps_data[4] == 'G' )
+//					{
+//						
+//					}
+//					Driver_USART1.Receive(&gps_data[i],1); 
+}			
+
+
+
+
+int main (void)
+{
+	char  MESSAGE_ID [7], UTC_TIME [11], LATITUDE[10],LONGITUDE[11], NS[2]; 
 	Init_UART();
 	GLCD_Initialize();
 	GLCD_ClearScreen();
-  GLCD_SetFont(&GLCD_Font_16x24);
+	GLCD_SetFont(&GLCD_Font_16x24);
 	
-//#ifdef RTE_CMSIS_RTOS2	// A commenter si utilisation RTOS
-//  /* Initialize CMSIS-RTOS2 */
-//  osKernelInitialize ();
+	//GLCD_DrawString(0,3*24,"debut");
 
-//  /* Create thread functions that start executing, 
-//  Example: osThreadNew(app_main, NULL, NULL); */
-
-//  /* Start thread execution */
-//  osKernelStart();
-//#endif
-
-  /* Infinite loop */
-	
-	while (1){
+  while (1) {
+		i=0;
+		Driver_USART1.Receive(dataGPS,1); 
 		
-		do
-		{
-			Driver_USART1.Receive(test,1); // tableau de 1 case
-			while (Driver_USART1.GetRxCount() <1 ); // on attend que 1 case soit pleine
-//			GLCD_DrawString(10,34,test);
-			
-	  }while(test[0] != '$');
-		//GLCD_DrawString(10,34,"ok");
-		for (i=0; dataGPS[i] != '*';i++){
-			Driver_USART1.Receive(dataGPS+i ,1); 
-			while (Driver_USART1.GetRxCount() <1 );// on attend que 100 case soit pleine
-			GLCD_DrawString(10,34,dataGPS);
-			GLCD_DrawString(10,34,"ok");
-			
-		}
-		GLCD_DrawString(10,54,"ok1");
-		traitementGPS(dataGPS);
-		
-		for (i = 0; i < 100; i++) {
-				dataGPS[i] = 0;
+		while(fin_drapeau != 1);
+		//GLCD_DrawString(0,3*24,"fin");
+		sscanf ( dataGPS, "%s %s %s %s %s", MESSAGE_ID,UTC_TIME,LATITUDE,NS,LONGITUDE);
+		if (strncmp(dataGPS, "$GPGGA", 6) == 0) 
+			{ 
+
+				GLCD_DrawString(0,1*24,MESSAGE_ID);
+				GLCD_DrawString(0,2*24,UTC_TIME);
+				GLCD_DrawString(0,3*24,LATITUDE);
+				GLCD_DrawString(0,4*24,NS);
+				GLCD_DrawString(0,5*24,LONGITUDE);
 			}
-//			dataGPS[i]=test2[0];        
-//		do
-//		{			
-//				
-//				i++;
-//			
-//		}while(test2[0]!= '*');
-////			GLCD_DrawString(10,100,"aaa");
-//				traitementGPS(dataGPS);
+		fin_drapeau = 0;
 		
-	}	
 	
+	}
+	
+
 }
